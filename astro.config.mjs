3a107faa19a6b1261472a41023f07c0fd4f32abb
Kurokishi592 <kennethwongcunwi@gmail.com>
2026-02-05 03:02:18 +0800

    Change all projects to mdx files and install astro plug ins. Updated current project details as well.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkCallout from 'remark-callout';
 
 export default defineConfig({
   site: 'https://kennethwongcunwi.com',
-  output: 'static'
+  output: 'static',
+  markdown: {
+    remarkPlugins: [remarkCallout]
+  },
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkCallout]
+    })
+  ]
 });
